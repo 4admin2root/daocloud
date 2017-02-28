214696cb9159c5fcf4e3b45426d3a63f5277a5e1
jason <lvzhijun@synjones.com>
2017-02-28 10:27:55 +0800

    daocloud:flask frontui

--- entry.py
+++ entry.py
@@ -1,16 +1,31 @@
-from flask import Flask, request
+import os
+import httplib
+from flask import Flask, request, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello():
-	return "Hello World!"
-@app.route("/power/<int:base>/<int:index>")
-def power(base, index):
-	return "%d" % (base ** index)
-@app.route("/addition/<int:x>/<int:y>")
-def add(x, y):
-	return "%d" % (x+y)
-@app.route("/substraction/<int:x>/<int:y>")
-def substract(x, y):
-	return "%d" % (x-y)
+def index():
+	return render_template('index.html')
+@app.route("/add", methods=['POST'])
+def add():
+    #
+    # from POST parameters
+    #
+    x = int(request.form['x'])
+    y = int(request.form['y'])
+    #
+    # from Kubernetes Service(environment variables)
+    #
+    my_calc_host = os.environ['MY_CALC_SERVICE_SERVICE_HOST']
+    my_calc_port = os.environ['MY_CALC_SERVICE_SERVICE_PORT']
+    #
+    # remote procedure call to MicroServices(my-calc)
+    #
+    client = httplib.HTTPConnection(my_calc_host,
+    my_calc_port)
+    client.request("GET", "/addition/%d/%d" % (x, y))
+    response = client.getresponse()
+    result = response.read()
+    return render_template('index.html',add_x=x, add_y=y, add_result=result)
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
