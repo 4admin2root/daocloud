8f6ee519f5334f1eebd9bf510c6862696339189d
jason <lvzhijun@synjones.com>
2017-08-16 15:57:01 +0800

    cleanup

--- roles/kubeadm_init/files/export.sh
+++
@@ -1,4 +0,0 @@
-export KUBE_REPO_PREFIX=reg-i.xzxpay.com/google-containers \
-       KUBE_HYPERKUBE_IMAGE=reg-i.xzxpay.com/google-containers/hyperkube-amd64:v1.6.1 \
-       #KUBE_DISCOVERY_IMAGE=registry.cn-hangzhou.aliyuncs.com/google-containers/kube-discovery-amd64:1.0 \
-       KUBE_ETCD_IMAGE=reg-i.xzxpay.com/google-containers/etcd-amd64:3.0.17
