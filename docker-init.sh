d560393e2b31961953ab71609928a5f50315ac68
jason <lvzhijun@synjones.com>
2017-08-16 16:27:14 +0800

    clean up unnecessary

--- docker-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i 's/10.8.32.30:8080/mirrors\.aliyun\.com/' /etc/yum.repos.d/CentOS-Base.repo
-sed -i 's/10.8.32.30:8080/mirrors\.aliyun\.com/' /etc/yum.repos.d/epel.repo
-yum install docker-1.12.6 -y
-systemctl enable docker
-sed -i 's/ExecStart=\/usr\/bin\/dockerd-current/ExecStart=\/usr\/bin\/dockerd-current --registry-mirror=https:\/\/m1empwb1.mirror.aliyuncs.com/' /usr/lib/systemd/system/docker.service
-systemctl daemon-reload
-systemctl restart docker
